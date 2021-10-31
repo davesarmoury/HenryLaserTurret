b96d36957e80f56853c005154894644cdbaf2bd8
Dave Niewinski <davesarmoury@gmail.com>
2021-10-31 17:14:38 -0400

    Updates

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-cd yolov5
-pip3 install -r requirements.txt
-
 sudo apt install -y curl unzip
+
 curl -L "https://app.roboflow.com/ds/7F1h3sKf7v?key=Ebf1rVDoQj" > roboflow.zip
 unzip roboflow.zip
 rm roboflow.zip
+
+cd yolov5
+pip3 install -r requirements.txt
 
--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd yolov5/
-python train.py --img 416 --batch 16 --epochs 50 --data '../data.yaml' --cfg ../yolo.yaml --weights '' --name yolov5s_results  --cache
+python3 train.py --img 416 --batch 16 --epochs 50 --data '../data.yaml' --cfg ../yolo.cfg --weights '' --name yolov5s_results  --cache
